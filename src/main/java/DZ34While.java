3addef1322dc605df7d61fe48c15b90791c47958
HP <tsybinsasha5@gmail.com>
2021-12-17 17:35:01 +0300

    project update

--- src/main/java/DZ34While.java
+++ src/main/java/DZ34While.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class DZ34While {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int ost ;
+        do {
+            ost=N%10;
+            N=N/10;
+
+            if (ost==2) {
+                System.out.println(true);
+                return;
+            }
+            System.out.println(false);
+            return;
+        } while (N!=0);
 
     }
 }
