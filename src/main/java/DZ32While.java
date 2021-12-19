71051e03e8a94c338428980254565eecd196db95
HP <tsybinsasha5@gmail.com>
2021-12-19 23:01:41 +0300

    added the last cycle while

--- src/main/java/DZ32While.java
+++ src/main/java/DZ32While.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class DZ32While {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int A,B;
+        System.out.println("A: ");
+        A = scanner.nextInt();
+
+        System.out.println("B: ");
+        B = scanner.nextInt();
+
+        if(A>B) {
+            while (A >= 0) {
+                A -= B;
+            }
+            System.out.println(A + B+" -Не занятые части");
+        }else {
+            System.out.println("Ввели неверные значения!");
+        }
     }
 }
+
--- src/main/java/DZ33While.java
+++ src/main/java/DZ33While.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class DZ33While {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int i = 0;
+        int A, B;
+
+
+
+        System.out.println("A: ");
+        A = scanner.nextInt();
+
+        System.out.println("B: ");
+        B = scanner.nextInt();
+        int n = B;
+
+        if(A > B){
+            while (n <= A){
+             n += B;
+             i++;
+            }
+            System.out.println("Колличество отрезко B на длинне А: "+i);
+        }
     }
 }
