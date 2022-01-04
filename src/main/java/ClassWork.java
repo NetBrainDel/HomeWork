0f16240faffa61444817e55f2d5a0dbfdad8481b
HP <tsybinsasha5@gmail.com>
2022-01-04 21:32:21 +0300

    add ClassWork

--- src/main/java/ClassWork.java
+++ src/main/java/ClassWork.java
@@ -34,15 +34,83 @@ public class ClassWork {
 
 
 
-        int[] array = {1, 4, 6, 0, 9};
+//        int[] array = {1, 4, 6, 0, 9};
+//
+//        System.out.println(Arrays.stream(array).min());
+//
+//        for (int i = 1; i < array.length; i++) {
+//         //   System.out.println(array[i]);
+//            if (array[i] < array[i-1]) {
+//                    System.out.println(array[i]);
+//            }
+//        }
 
-        System.out.println(Arrays.stream(array).min());
+        /*1 ведите число 1 2 3 проверка ввода*/
 
-        for (int i = 1; i < array.length; i++) {
-         //   System.out.println(array[i]);
-            if (array[i] < array[i-1]) {
-                    System.out.println(array[i]);
-            }
-        }
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int n = scanner.nextInt();
+//
+//        switch (n){
+//            case 1:
+//                System.out.println(1);
+//                break;
+//
+//            case 2:
+//                System.out.println(2);
+//                break;
+//
+//            case 3:
+//                System.out.println(3);
+//                break;
+//
+//            default:
+//                System.out.println("ef");
+//        }
+
+        /*2 найти количество отриц и полож числа*/
+
+//        int a = 1;
+//        int b = -2;
+//        int c = 3;
+//        int pol = 0;
+//        int otr = 0;
+//
+//        if (a>0){
+//            pol++;
+//        }else {
+//            otr++;
+//        }
+//        if (b>0){
+//            pol++;
+//        }else {
+//            otr++;
+//        }
+//        if (c>0){
+//            pol++;
+//        }else {
+//            otr++;
+//        }
+//        System.out.println(pol);
+//        System.out.println(otr);
+
+        /*3 вывести максимальный и минимальный элемент массива*/
+
+//        int[] arr= {2,3,4,5,6};
+//        int max = arr[0];
+//        int min = arr[0];
+//
+//        for (int i = 1; i < arr.length; i++) {
+//            if (max < arr[i]){
+//                max = arr[i];
+//            }
+//            if (min > arr[i]){
+//                min = arr[i];
+//            }
+//        }
+//        System.out.println(max);
+//        System.out.println(min);
+
+
     }
 }
